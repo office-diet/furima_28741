13080ae8551ec1275dd9694b64745e1bb15bf6e6
office-diet <kw278u@bma.biglobe.ne.jp>
2020-08-01 20:00:07 +0900

    Userモデルバリデーション修正_04

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :birthday, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  JAPANESE_REGEX = /\A[ぁ-んァ-ン一-龥]/.freeze
+  KATAKANA_REGEX  = /\A[ァ-ヶー－]/.freeze
+  
   with_options presence: true do
+    validates :nickname, uniqueness: { case_sensitive: true, message: 'has already been taken' }
     validates :email, uniqueness: { case_sensitive: true, message: 'has already been taken' }
-    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i, message: 'input half-width numbers and characters.' }
-    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'input full-width japanese characters.' }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'input full-width japanese characters.' }
-    validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'input full-width katakana characters.' }
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'input full-width katakana characters.' }
+    validates :password, format: { with: PASSWORD_REGEX, message: 'input half-width numbers and characters.' }
+    validates :family_name, format: { with: JAPANESE_REGEX, message: 'input full-width japanese characters.' }
+    validates :first_name, format: { with: JAPANESE_REGEX, message: 'input full-width japanese characters.' }
+    validates :family_name_kana, format: { with: KATAKANA_REGEX, message: 'input full-width katakana characters.' }
+    validates :first_name_kana, format: { with: KATAKANA_REGEX, message: 'input full-width katakana characters.' }
+    validates :birthday
     validates :birthday_valid?
   end
 
