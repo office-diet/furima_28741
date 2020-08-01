7cab4d38bbe4648d53fda0592fed5d49bc7b6ebf
office-diet <kw278u@bma.biglobe.ne.jp>
2020-08-01 17:01:13 +0900

    テストコード完成　バリデーション 修正03

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,23 +6,22 @@ class User < ApplicationRecord
   
          
   validates :nickname, presence: true
+  validates :birthday, presence: true
   with_options presence: true do
-    validates :email, format: { with: /\A.+@.+\z/, message: "@を含むメールアドレスを入力してください。"}
-    validates :email, uniqueness: { case_sensitive: true, message: "登録済みのメールアドレスです。"}
-    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i, message: "半角英数混合、６文字以上としてください。"}
-    
-    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角漢字・ひらがな・カタカナで入力してください。"}
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角漢字・ひらがな・カタカナで入力してください。"}
-    validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナで入力してください。"}
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナで入力してください。"}
-    validates :date_valid?
+    validates :email, uniqueness: { case_sensitive: true, message: "has already been taken"}
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i, message: "input half-width numbers and characters."}
+    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "input full-width japanese characters."}
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "input full-width japanese characters."}
+    validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "input full-width katakana characters."}
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "input full-width katakana characters."}
+    validates :birthday_valid?
   end
 
   private
-  def date_valid?
+  def birthday_valid?
     date = birthday.to_s
-    if date.count("-") > 3
-      errors.add(:birthday, "日付の値が不正です")  
+    if date != "" && date.count("-") > 2
+      errors.add(:birthday, "invalid")
     else
       return true
     end
