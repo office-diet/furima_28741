39b34f8b99f147b7b91b84f3a1626a34cd9fa956
office-diet <kw278u@bma.biglobe.ne.jp>
2020-08-01 21:12:36 +0900

    itemsテーブル定義

---
+++ db/migrate/20200801120108_create_items.rb
@@ -0,0 +1,17 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string      :name,            null: false
+      t.text        :text,            null: false
+      t.integer     :price,           null: false
+      t.references  :user,            null: false, foreign_key: true
+      t.references  :category,        null: false, foreign_key: true
+      t.references  :condition,       null: false, foreign_key: true
+      t.references  :postage,         null: false, foreign_key: true
+      t.references  :prefcture,       null: false, foreign_key: true
+      t.references  :shipment_delay,  null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
