fa868874c9dbfe69d1e8f16f3a812809c50ac54a
office-diet <kw278u@bma.biglobe.ne.jp>
2020-08-01 21:56:29 +0900

    ActiveHash定義

--- db/migrate/20200801120108_create_items.rb
+++ db/migrate/20200801120108_create_items.rb
@@ -1,15 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.string      :name,            null: false
-      t.text        :text,            null: false
-      t.integer     :price,           null: false
-      t.bigint      :user_id,         null: false, foreign_key: true
-      t.references  :category,        null: false, foreign_key: true
-      t.references  :condition,       null: false, foreign_key: true
-      t.references  :postage,         null: false, foreign_key: true
-      t.references  :prefcture,       null: false, foreign_key: true
-      t.references  :shipment_delay,  null: false, foreign_key: true
+      t.string   :name,            null: false
+      t.text     :text,            null: false
+      t.integer  :price,           null: false
+      t.bigint   :user_id,            null: false, foreign_key: true
+      t.integer  :category_id,        null: false, foreign_key: true
+      t.integer  :condition_id,       null: false, foreign_key: true
+      t.integer  :postage_id,         null: false, foreign_key: true
+      t.integer  :prefcture_id,       null: false, foreign_key: true
+      t.integer  :shipment_delay_id,  null: false, foreign_key: true
 
       t.timestamps
     end
