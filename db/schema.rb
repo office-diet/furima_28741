12129994a6b256183543bcf657251065f101680f
office-diet <kw278u@bma.biglobe.ne.jp>
2020-08-02 10:31:35 +0900

    itemsテーブルdb:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_31_100703) do
+ActiveRecord::Schema.define(version: 2020_08_01_120108) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "text", null: false
+    t.integer "price", null: false
+    t.bigint "user_id", null: false
+    t.integer "category_id", null: false
+    t.integer "condition_id", null: false
+    t.integer "postage_id", null: false
+    t.integer "prefcture_id", null: false
+    t.integer "shipment_delay_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
